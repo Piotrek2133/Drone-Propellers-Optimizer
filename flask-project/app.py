7a3647458155ee3aea84514783a40c1d60892046
SpalarniaOdpaduwOrganicznych <247094@edu.p.lodz.pl>
2024-12-10 23:28:05 +0100

    twd-2

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "pakinstanekskie drony"
+def main_page():
+    return render_template('index.html')
+
+@app.route('/tryModel')
+def subpage1():
+    return render_template('tryModel.html')
+
+@app.route('/about')
+def subpage2():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+

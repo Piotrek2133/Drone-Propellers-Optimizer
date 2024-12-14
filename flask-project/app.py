c51a83184e215ab43b83c3232ff563c015d9337a
SpalarniaOdpaduwOrganicznych <247094@edu.p.lodz.pl>
2024-12-14 23:15:06 +0100

    versionBeta

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,50 +1,44 @@
-from flask import Flask, render_template, request, jsonify
-import joblib
-import numpy as np
-import os
+from flask import Flask, render_template, request
+from optimize import optimize
 
 app = Flask(__name__)
-
-# Load the model
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "model", "mock_model.joblib")
-try:
-    model = joblib.load(MODEL_PATH)
-except FileNotFoundError:
-    print("Model file not found. Continuing without the model.")
-    model = None
-
-# App part
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
-@app.route('/tryModel')
-def subpage1():
-    return render_template('tryModel.html')
-
 @app.route('/about')
 def subpage2():
     return render_template('about.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if not model:
-        return jsonify({'error': 'Model not loaded. Please make sure mock_model.joblib exists.'}), 500
+@app.route('/optimize', methods=['GET', 'POST'])
+def run_optimization():
+    if request.method == 'GET':
+        return render_template('optimize_form.html')
+    elif request.method == 'POST':
+        try:
+            inputB = float(request.form['inputB'])
+            inputD = float(request.form['inputD'])
+            inputP = float(request.form['inputP'])
+            inputJ = float(request.form['inputJ'])
+            inputN = float(request.form['inputN'])
+        except ValueError:
+            return render_template(
+                'optimize_form.html', 
+                error="Please ensure all inputs are valid numbers."
+            )
 
-    try:
-        data = request.json
-        if not data or 'inputs' not in data or len(data['inputs']) != 4:
-            return jsonify({'error': 'Invalid input. Please provide an array of 4 numbers.'}), 400
-
-        inputs = np.array(data['inputs']).reshape(1, -1)
-
-        prediction = model.predict(inputs)
-
-        return jsonify({'prediction': prediction.tolist()})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        input_params = [inputB, inputD, inputP, inputJ, inputN]
+        
+        best_individual, best_efficiency,v, T = optimize.optimize(input_params)
+        
+        return render_template(
+            'optimize_result.html',
+            best_individual=best_individual,
+            best_efficiency=best_efficiency,
+            v=v,
+            T=T
+        )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    #app.run(debug=True)
